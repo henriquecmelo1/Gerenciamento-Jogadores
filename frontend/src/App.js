bc9ea8148f83a9ed83a993af6a340e0335b8b813
henriquecmelo1 <hcm@cin.ufpe.br>
2024-12-13 00:07:05 -0300

    feat: add initial frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div 
+        className='mt-3 justify-content-center align-items-center mx-auto'
+        style={{width: '70vw', backgroundColor: '#ffffff'}}
+      >
+
+        <h2 className='text-center text-white bg-success card mb-1'>Gerenciamento de Jogadores</h2>
+        <h6 className='text-center text-white bg-success card mb-1 pb-1'>Informações dos Jogadores</h6>
+      </div>
+    </div>
+  );
+}
+
+export default App;
